cfbe91f79a29844d2df8a2fa55bd6899344563e9
Maxim Yakovlev <it.trainee@yandex.ru>
2025-07-03 19:41:44 +0300

    performing and processing a GET request using axios

--- index.js
+++ index.js
@@ -37,3 +37,8 @@ getFamilyMember(1)
     .catch((err) => {
         console.log(err);
     });
+
+axios
+    .get('https://jsonplaceholder.typicode.com/todos/1')
+    .then((response) => response.headers)
+    .then((headers) => console.log(headers));
