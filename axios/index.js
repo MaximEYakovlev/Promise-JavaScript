26e55daba07de58e94dc1522cd78db42682acdf4
Maxim Yakovlev <it.trainee@yandex.ru>
2025-07-06 17:39:15 +0300

    added error handling

--- axios/index.js
+++ axios/index.js
@@ -3,4 +3,12 @@ import axios from 'axios';
 axios
     .get('https://jsonplaceholder.typicode.com/todos/1')
     .then((response) => response.headers)
-    .then((headers) => console.log(headers));
+    .then((headers) => console.log(headers))
+    .catch((error) => {
+        // handle error
+        console.log(error);
+    })
+    .finally(() => {
+        // always executed
+        console.log('done');
+    });
