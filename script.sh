452a9d51c5d6f2fc8656749386d4783951ea670a
Maxim Yakovlev <it.trainee@yandex.ru>
2025-07-11 19:42:33 +0300

    added menu in bash

--- script.sh
+++ script.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-echo "Start"
 docker-compose -f docker-compose.yml up -d
+# Bash Menu Script
+PS3='Please enter your choice: '
+options=("Option 1" "Option 2" "Option 3" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Option 1")
+            echo "you chose choice 1"
+            ;;
+        "Option 2")
+            echo "you chose choice 2"
+            ;;
+        "Option 3")
+            echo "you chose choice $REPLY which is $opt"
+            ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
