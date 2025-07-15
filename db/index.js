17ef5ba79a3acefa1ef293f5071d1158c239fac4
Maxim Yakovlev <it.trainee@yandex.ru>
2025-07-15 05:14:16 +0300

    connection pool using

--- db/index.js
+++ db/index.js
@@ -1,14 +1,19 @@
-import { Client } from 'pg';
-const client = new Client();
-await client.connect();
+// import { Client } from 'pg';
+// const client = new Client();
+// await client.connect();
 
-try {
-    const res = await client.query('SELECT $1::text as message', [
-        'Hello world!',
-    ]);
-    console.log(res.rows[0].message); // Hello world!
-} catch (error) {
-    console.error(error);
-} finally {
-    await client.end();
-}
+// try {
+//     const res = await client.query('SELECT $1::text as message', [
+//         'Hello world!',
+//     ]);
+//     console.log(res.rows[0].message); // Hello world!
+// } catch (error) {
+//     console.error(error);
+// } finally {
+//     await client.end();
+// }
+
+import { Pool } from 'pg';
+const pool = new Pool();
+const res = await pool.query('SELECT $1::text as message', ['Hello world!']);
+console.log(res.rows[0].message); // Hello world!
