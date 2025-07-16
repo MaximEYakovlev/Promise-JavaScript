4326343b87340a30b3e029ef9676358d1f1172f5
Maxim Yakovlev <it.trainee@yandex.ru>
2025-07-16 03:56:42 +0300

    reusable pool of clients, check out, use, and return

--- db/index.js
+++ db/index.js
@@ -13,7 +13,25 @@
 //     await client.end();
 // }
 
-import { Pool } from 'pg';
-const pool = new Pool();
-const res = await pool.query('SELECT $1::text as message', ['Hello world!']);
-console.log(res.rows[0].message); // Hello world!
+// import { Pool } from 'pg';
+// const pool = new Pool();
+// const res = await pool.query('SELECT $1::text as message', ['Hello world!']);
+// console.log(res.rows[0].message); // Hello world!
+
+import pg from 'pg'
+const { Pool } = pg
+ 
+const pool = new Pool()
+ 
+// the pool will emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on('error', (err, client) => {
+  console.error('Unexpected error on idle client', err)
+  process.exit(-1)
+})
+ 
+const client = await pool.connect()
+const res = await client.query('SELECT * FROM users WHERE id = $1', [1])
+console.log(res.rows[0])
+ 
+client.release()
