a173c832fb3ed60c692bd39841860dd395cc614d
Maxim Yakovlev <it.trainee@yandex.ru>
2025-07-17 05:12:31 +0300

    shut down a pool

--- db/index.js
+++ db/index.js
@@ -18,20 +18,37 @@
 // const res = await pool.query('SELECT $1::text as message', ['Hello world!']);
 // console.log(res.rows[0].message); // Hello world!
 
+// import pg from 'pg'
+// const { Pool } = pg
+ 
+// const pool = new Pool()
+ 
+// // the pool will emit an error on behalf of any idle clients
+// // it contains if a backend error or network partition happens
+// pool.on('error', (err, client) => {
+//   console.error('Unexpected error on idle client', err)
+//   process.exit(-1)
+// })
+ 
+// const client = await pool.connect()
+// const res = await client.query('SELECT * FROM users WHERE id = $1', [1])
+// console.log(res.rows[0])
+ 
+// client.release()
+
 import pg from 'pg'
 const { Pool } = pg
- 
 const pool = new Pool()
  
-// the pool will emit an error on behalf of any idle clients
-// it contains if a backend error or network partition happens
-pool.on('error', (err, client) => {
-  console.error('Unexpected error on idle client', err)
-  process.exit(-1)
+console.log('starting async query')
+const result = await pool.query('SELECT NOW()')
+console.log('async query finished')
+ 
+console.log('starting callback query')
+pool.query('SELECT NOW()', (err, res) => {
+  console.log('callback query finished')
 })
  
-const client = await pool.connect()
-const res = await client.query('SELECT * FROM users WHERE id = $1', [1])
-console.log(res.rows[0])
- 
-client.release()
+console.log('calling end')
+await pool.end()
+console.log('pool has drained')
