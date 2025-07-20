68f18771f0e2a02b0b696f45713a00c3a0652b50
Maxim Yakovlev <it.trainee@yandex.ru>
2025-07-20 05:48:24 +0300

    added action.sh file / functions for using in script.sh

---
+++ acrion.sh
@@ -0,0 +1,11 @@
+start_db() {
+    echo "db started"
+}
+
+start_front() {
+    echo "front started"
+}
+
+start_back() {
+    echo "back started"
+}
--- db/index.js
+++ db/index.js
@@ -20,35 +20,35 @@
 
 // import pg from 'pg'
 // const { Pool } = pg
- 
+
 // const pool = new Pool()
- 
+
 // // the pool will emit an error on behalf of any idle clients
 // // it contains if a backend error or network partition happens
 // pool.on('error', (err, client) => {
 //   console.error('Unexpected error on idle client', err)
 //   process.exit(-1)
 // })
- 
+
 // const client = await pool.connect()
 // const res = await client.query('SELECT * FROM users WHERE id = $1', [1])
 // console.log(res.rows[0])
- 
+
 // client.release()
 
-import pg from 'pg'
-const { Pool } = pg
-const pool = new Pool()
- 
-console.log('starting async query')
-const result = await pool.query('SELECT NOW()')
-console.log('async query finished')
- 
-console.log('starting callback query')
+import pg from 'pg';
+const { Pool } = pg;
+const pool = new Pool();
+
+console.log('starting async query');
+const result = await pool.query('SELECT NOW()');
+console.log('async query finished');
+
+console.log('starting callback query');
 pool.query('SELECT NOW()', (err, res) => {
-  console.log('callback query finished')
-})
- 
-console.log('calling end')
-await pool.end()
-console.log('pool has drained')
+    console.log('callback query finished');
+});
+
+console.log('calling end');
+await pool.end();
+console.log('pool has drained');
